5f4c719768c17ba5ca4d91c80e9d31a446923f42
Simon HARVEY <simon.harvey.86@gmail.com>
2024-01-18 09:21:20 -0500

    Update emtrypoint.sh syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,19 @@ set -e
 create_pid_dir() {
   mkdir -p /run/apt-cacher-ng
   chmod -R 0755 /run/apt-cacher-ng
-  chown ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} /run/apt-cacher-ng
+  chown "${APT_CACHER_NG_USER}":"${APT_CACHER_NG_USER}" /run/apt-cacher-ng
 }
 
 create_cache_dir() {
-  mkdir -p ${APT_CACHER_NG_CACHE_DIR}
-  chmod -R 0755 ${APT_CACHER_NG_CACHE_DIR}
-  chown -R ${APT_CACHER_NG_USER}:root ${APT_CACHER_NG_CACHE_DIR}
+  mkdir -p "${APT_CACHER_NG_CACHE_DIR}"
+  chmod -R 0755 "${APT_CACHER_NG_CACHE_DIR}"
+  chown -R "${APT_CACHER_NG_USER}":root "${APT_CACHER_NG_CACHE_DIR}"
 }
 
 create_log_dir() {
-  mkdir -p ${APT_CACHER_NG_LOG_DIR}
-  chmod -R 0755 ${APT_CACHER_NG_LOG_DIR}
-  chown -R ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} ${APT_CACHER_NG_LOG_DIR}
+  mkdir -p "${APT_CACHER_NG_LOG_DIR}"
+  chmod -R 0755 "${APT_CACHER_NG_LOG_DIR}"
+  chown -R "${APT_CACHER_NG_USER}":"${APT_CACHER_NG_USER}" "${APT_CACHER_NG_LOG_DIR}"
 }
 
 create_pid_dir
@@ -24,23 +24,23 @@ create_cache_dir
 create_log_dir
 
 #if user config directory was mounted / exists copy files to main apt-cacher-ng directory
-if [ -d "$APT_CACHER_NG_CONFIG_DIR" ]; then;
-cp ${APT_CACHER_NG_CONFIG_DIR}/.  /etc/apt-cacher-ng/
+if [ -d "$APT_CACHER_NG_CONFIG_DIR" ]; then
+  cp "${APT_CACHER_NG_CONFIG_DIR}"/.  /etc/apt-cacher-ng/
 fi
 
 # allow arguments to be passed to apt-cacher-ng
 if [[ ${1:0:1} = '-' ]]; then
-  EXTRA_ARGS="$@"
+  EXTRA_ARGS="$*"
   set --
 elif [[ ${1} == apt-cacher-ng || ${1} == $(command -v apt-cacher-ng) ]]; then
-  EXTRA_ARGS="${@:2}"
+  EXTRA_ARGS="${*:2}"
   set --
 fi
 
 # default behaviour is to launch apt-cacher-ng
 if [[ -z ${1} ]]; then
-  exec start-stop-daemon --start --chuid ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} \
-    --exec "$(command -v apt-cacher-ng)" -- -c /etc/apt-cacher-ng ${EXTRA_ARGS}
+  exec start-stop-daemon --start --chuid "${APT_CACHER_NG_USER}":"${APT_CACHER_NG_USER}" \
+    --exec "$(command -v apt-cacher-ng)" -- -c /etc/apt-cacher-ng "${EXTRA_ARGS}"
 else
   exec "$@"
 fi
